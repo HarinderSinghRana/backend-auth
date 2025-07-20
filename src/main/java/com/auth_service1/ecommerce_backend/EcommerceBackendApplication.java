7f7bc236ff111fcc1a725144d6c5af1c68ed6fae
Harinder SIngh Rana <harinder.rana1998@gmail.com>
2025-07-20 17:10:58 -0400

    chore: initialize Spring Boot project with Web, Security, DevTools, JDBC, JPA, and Lombok

---
+++ src/main/java/com/auth_service1/ecommerce_backend/EcommerceBackendApplication.java
@@ -0,0 +1,14 @@
+package com.auth_service1.ecommerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		System.out.println("This is a first draft page");
+		SpringApplication.run(EcommerceBackendApplication.class, args);
+	}
+
+}
