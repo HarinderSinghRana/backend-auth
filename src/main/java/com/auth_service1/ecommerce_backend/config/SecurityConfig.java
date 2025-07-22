40df54fec162807b195e98b5e6a2a733db9494bf
Harinder SIngh Rana <harinder.rana1998@gmail.com>
2025-07-22 16:22:58 -0400

    chore: Integrate Prometheus monitoring with Spring Boot Actuator
    - Added micrometer-registry-prometheus dependency
    - Enabled actuator and Prometheus metrics endpoint
    - Exposed /actuator/prometheus for Prometheus scraping

--- src/main/java/com/auth_service1/ecommerce_backend/config/SecurityConfig.java
+++ src/main/java/com/auth_service1/ecommerce_backend/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**", "/api/users/register").permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
