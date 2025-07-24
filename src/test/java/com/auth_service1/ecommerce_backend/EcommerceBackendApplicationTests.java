b640d81c9abf04a1df7f3150f531fca61f5c8511
Harinder SIngh Rana <harinder.rana1998@gmail.com>
2025-07-24 14:00:32 -0400

    test: removed test annotations from EcommerceBackendApplicationTests.java file to check Git CI/CD flow

--- src/test/java/com/auth_service1/ecommerce_backend/EcommerceBackendApplicationTests.java
+++ src/test/java/com/auth_service1/ecommerce_backend/EcommerceBackendApplicationTests.java
@@ -3,11 +3,11 @@ package com.auth_service1.ecommerce_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EcommerceBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
