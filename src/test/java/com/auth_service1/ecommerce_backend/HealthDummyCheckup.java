a3c032d9a3c1ddc81aa6717e6bb82f08fcee0ae8
Harinder SIngh Rana <harinder.rana1998@gmail.com>
2025-07-24 13:58:08 -0400

    test: added dummy HealthDummyCheckup.java file to check Git CI/CD flow

---
+++ src/test/java/com/auth_service1/ecommerce_backend/HealthDummyCheckup.java
@@ -0,0 +1,13 @@
+package com.auth_service1.ecommerce_backend;
+
+import org.junit.jupiter.api.Test;
+
+public class HealthDummyCheckup {
+
+    @Test
+    void simpleTest() {
+        // CI sanity test
+        assert true;
+    }
+
+}
